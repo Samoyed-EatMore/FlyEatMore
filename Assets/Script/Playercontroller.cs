80a724cfc7dc28293066691c97d77d8db92c3f4e
wangxin <14302010064@fudan.edu.cn>
2017-02-10 23:55:55 +0800

    create the basic scene;create a game object and implement its basic control

---
+++ Assets/Script/Playercontroller.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour {
+
+
+	Vector3 oldPos;// 记录位置
+	public float speed;// 前进速度
+	public float degree;// 旋转角
+	private Rigidbody rb;
+	//private int count;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		//		count = 0;
+		//		SetCountText ();
+		//		winText.text = "";
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		// 转弯
+		if (moveHorizontal > 0) {
+			rb.transform.Rotate (Vector3.up * degree);
+		}
+		if (moveHorizontal < 0) {
+			rb.transform.Rotate (Vector3.up * -degree);
+		}
+		if (moveVertical > 0) {
+			rb.transform.Rotate (Vector3.left * degree);
+		}
+		if (moveVertical < 0) {
+			rb.transform.Rotate (Vector3.left * -degree);
+		}
+
+		oldPos = transform.position;//记录位置
+		rb.transform.Translate(Vector3.forward * speed * Time.deltaTime);//移动
+
+	}
+
+	// 给其他物体加上标签，本物体（与其他物体）碰撞时，实现相应动作
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Food")) {
+			other.gameObject.SetActive (false);
+
+			//	count = count + 1;
+			//	SetCountText ();
+		} else if (other.gameObject.CompareTag ("barrier")) {
+			// 
+		}
+			
+	}
+		
+}
