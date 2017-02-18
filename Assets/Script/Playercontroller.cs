0f4a135849ee8d8bc87b0dc7a2c5814780a45fa5
wangxin <14302010064@fudan.edu.cn>
2017-02-19 00:24:45 +0800

    add a boundary to avoid fly too high.

--- Assets/Script/Playercontroller.cs
+++ Assets/Script/Playercontroller.cs
@@ -9,8 +9,11 @@ public class Playercontroller : MonoBehaviour {
 	private float degree = 2;// 旋转角
 	private float dSpeed = 5;// 每次加速的变量，可设置为合理值
 	private float maxSpeed = 30;// 最大速度，可设置为合理值
+	private float timeBefore = 0;
+	private float maxPos = 75;// 最高位置
 	public Text countText;
 	public Text gameText;// 游戏结果
+	private bool isOutOfBound = false;// 边界
 
 	private Rigidbody rb;
 	private int count;
@@ -22,6 +25,7 @@ public class Playercontroller : MonoBehaviour {
 		rb = GetComponent<Rigidbody>();
 		count = 0;
 		SetCountText ();
+		timeBefore = Time.time;
 	}
 
 	void FixedUpdate ()
@@ -47,6 +51,25 @@ public class Playercontroller : MonoBehaviour {
 		    rb.transform.Translate(Vector3.forward * speed * Time.deltaTime);//移动
 		}
 
+		if (isOutOfBound) {
+			if (Time.time - timeBefore == 1) {
+				gameText.text = "You'll lose in 3 seconds!";
+			} else if (Time.time - timeBefore == 2) {
+				gameText.text = "You'll lose in 2 seconds!";
+			} else if (Time.time - timeBefore == 3) {
+				gameText.text = "You'll lose in 1 seconds!";
+			} else if (Time.time - timeBefore == 4){
+				gameText.text = "You'll lose in 0 seconds!";
+			} else if (Time.time - timeBefore > 5){
+				if (rb.position.y > maxPos) { // confirm again
+					GameOver ();
+				} else {
+					isOutOfBound = false;
+					ClearGameText ();
+				}
+			}
+		}
+
 	}
 
 	// 给其他物体加上标签，本物体（与其他物体）碰撞时，实现相应动作
@@ -58,8 +81,10 @@ public class Playercontroller : MonoBehaviour {
 			SetCountText ();
 			SpeedUp ();
 		} else if (other.gameObject.CompareTag ("barrier")) {
-			GameOver();
-		}			
+			GameOver ();
+		} else if (other.gameObject.CompareTag ("Boundary")) {
+			OutOfBoundaryWarning ();
+		}
 	}
 
 	void SetCountText ()
@@ -75,6 +100,20 @@ public class Playercontroller : MonoBehaviour {
 		}
 	}
 
+	// Out of Boundary 
+	void OutOfBoundaryWarning () {
+		isOutOfBound = !isOutOfBound;
+		if (isOutOfBound) {
+			gameText.text = "You'll lose the game \n if you keep on \n flying too high!";
+			timeBefore = Time.time;
+		} else {
+			ClearGameText ();
+		}
+	}
+
+	void ClearGameText(){
+		gameText.text = "";
+	}
 	// Game over.
 	void GameOver ()
 	{
