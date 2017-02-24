fb1c7f572c4c1c875d597c609e7ba326b4f58de2
wangxin <14302010064@fudan.edu.cn>
2017-02-24 19:36:35 +0800

    add the start scene and the help scene.

---
+++ Assets/Script/ButtonClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonClick : MonoBehaviour {
+   
+	// Help Button click
+	public void  clickForHelp () {
+	    SceneManager.LoadSceneAsync("HelpScene");
+    }
+
+    // Start Button click
+	public void clickForStart () { 
+	    SceneManager.LoadSceneAsync("GameScene");
+    }
+
+    // Return Button click
+	public void clickForReturn () { 
+	    SceneManager.LoadSceneAsync("StartScene");
+    }
+}
