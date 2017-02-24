ec144265f657ea09c964216c4ca8e8cb54c15764
wangxin <14302010064@fudan.edu.cn>
2017-02-24 19:16:34 +0800

    fix the start flying.

--- Assets/Script/Playercontroller.cs
+++ Assets/Script/Playercontroller.cs
@@ -26,104 +26,110 @@ public class Playercontroller : MonoBehaviour {
 	private bool isOutOfBound = false;// 边界
 	private bool isAttack = false;// 攻击
 
+	public GameObject targetCamera;
+
 	void Start()
 	{
-		rb = GetComponent<Rigidbody>();
-		anim.SetBool ("isDead", false);
-		anim.SetBool ("isAttack", false);
-		count = 0;
-		SetCountText ();
-		timeOutBefore = Time.time;
-		timeMoveBefore = Time.time;
+			rb = GetComponent<Rigidbody> ();
+			anim.SetBool ("isDead", false);
+			anim.SetBool ("isAttack", false);
+			count = 0;
+			SetCountText ();
+			timeOutBefore = Time.time;
+			timeMoveBefore = Time.time;
 	}
 
 	void FixedUpdate ()
 	{
-		moveHorizontal = Input.GetAxis ("Horizontal");
-		moveVertical = Input.GetAxis ("Vertical");
-		if (moveVertical > 0) {
-			totalDegree += degree;
-			rb.transform.Rotate (Vector3.left * degree);
-		}
-		if (moveVertical < 0) {
-			totalDegree -= degree;
-			rb.transform.Rotate (Vector3.left * -degree);
-		}
-
-		// 左右转弯，首先保持水平
-		if (totalDegree == 0) {
+		if (targetCamera.activeInHierarchy) {
 			moveHorizontal = Input.GetAxis ("Horizontal");
-			// 左右转弯
-			if (moveHorizontal > 0) {
-				rb.transform.Rotate (Vector3.up * degree);
+			moveVertical = Input.GetAxis ("Vertical");
+			if (moveVertical > 0) {
+				totalDegree += degree;
+				rb.transform.Rotate (Vector3.left * degree);
 			}
-			if (moveHorizontal < 0) {
-				rb.transform.Rotate (Vector3.up * -degree);
+			if (moveVertical < 0) {
+				totalDegree -= degree;
+				rb.transform.Rotate (Vector3.left * -degree);
 			}
-			timeMoveBefore = Time.time;
-		} else {// 方向非水平，需调整
-			if (Time.time - timeMoveBefore > 0.05) {
-				if (totalDegree > 0) {
-					totalDegree -= degree;
-					rb.transform.Rotate (Vector3.left * -degree);
+
+			// 左右转弯，首先保持水平
+			if (totalDegree == 0) {
+				moveHorizontal = Input.GetAxis ("Horizontal");
+				// 左右转弯
+				if (moveHorizontal > 0) {
+					rb.transform.Rotate (Vector3.up * degree);
 				}
-				if (totalDegree < 0) {
-					totalDegree += degree;
-					rb.transform.Rotate (Vector3.left * degree);
+				if (moveHorizontal < 0) {
+					rb.transform.Rotate (Vector3.up * -degree);
 				}
 				timeMoveBefore = Time.time;
-			}
-		}
-
-		if(isMoving){
-			rb.transform.Translate(Vector3.forward * speed * Time.deltaTime);//移动
-		}
-
-		if (isOutOfBound) {
-			if (Time.time - timeOutBefore == 1) {
-				gameText.text = "You'll lose in 3 seconds!";
-			} else if (Time.time - timeOutBefore == 2) {
-				gameText.text = "You'll lose in 2 seconds!";
-			} else if (Time.time - timeOutBefore == 3) {
-				gameText.text = "You'll lose in 1 seconds!";
-			} else if (Time.time - timeOutBefore == 4){
-				gameText.text = "You'll lose in 0 seconds!";
-			} else if (Time.time - timeOutBefore > 5){
-				if (rb.position.y > maxPos) { // confirm again
-					GameOver ();
-				} else {
-					isOutOfBound = false;
-					ClearGameText ();
+			} else {// 方向非水平，需调整
+				if (Time.time - timeMoveBefore > 0.05) {
+					if (totalDegree > 0) {
+						totalDegree -= degree;
+						rb.transform.Rotate (Vector3.left * -degree);
+					}
+					if (totalDegree < 0) {
+						totalDegree += degree;
+						rb.transform.Rotate (Vector3.left * degree);
+					}
+					timeMoveBefore = Time.time;
 				}
 			}
-		}
 
-		if (isAttack && Time.time - timeAttackBefore > 0.5) {
-			anim.SetBool ("isAttack", false);
-			isAttack = false;
+			if (isMoving) {
+				rb.transform.Translate (Vector3.forward * speed * Time.deltaTime);//移动
+			}
+
+			if (isOutOfBound) {
+				if (Time.time - timeOutBefore == 1) {
+					gameText.text = "You'll lose in 3 seconds!";
+				} else if (Time.time - timeOutBefore == 2) {
+					gameText.text = "You'll lose in 2 seconds!";
+				} else if (Time.time - timeOutBefore == 3) {
+					gameText.text = "You'll lose in 1 seconds!";
+				} else if (Time.time - timeOutBefore == 4) {
+					gameText.text = "You'll lose in 0 seconds!";
+				} else if (Time.time - timeOutBefore > 5) {
+					if (rb.position.y > maxPos) { // confirm again
+						GameOver ();
+					} else {
+						isOutOfBound = false;
+						ClearGameText ();
+					}
+				}
+			}
+
+			if (isAttack && Time.time - timeAttackBefore > 0.5) {
+				anim.SetBool ("isAttack", false);
+				isAttack = false;
+			}
 		}
 	}
 
 	// 给其他物体加上标签，本物体（与其他物体）碰撞时，实现相应动作
 	void OnTriggerEnter(Collider other) 
-	{
-		if (other.gameObject.CompareTag ("Food")) {
-			anim.SetBool ("isAttack", true);
-			isAttack = true;
-			timeAttackBefore = Time.time;
-			other.gameObject.SetActive (false);
-			count = count + 1;
-			SetCountText ();
-			SpeedUp ();
-		} else if (other.gameObject.CompareTag ("barrier")) {
-			GameOver ();
-		} else if (other.gameObject.CompareTag ("Boundary")) {
-			OutOfBoundaryWarning ();
-		} else if (other.gameObject.CompareTag ("hazard")) {
-			Destroy (other.gameObject);
-			GameOver ();
-		} else if (other.gameObject.CompareTag ("MyOwnBoundary")) {
-			return;
+	{		
+		if (targetCamera.activeInHierarchy) {
+			if (other.gameObject.CompareTag ("Food")) {
+				anim.SetBool ("isAttack", true);
+				isAttack = true;
+				timeAttackBefore = Time.time;
+				other.gameObject.SetActive (false);
+				count = count + 1;
+				SetCountText ();
+				SpeedUp ();
+			} else if (other.gameObject.CompareTag ("barrier")) {
+				GameOver ();
+			} else if (other.gameObject.CompareTag ("Boundary")) {
+				OutOfBoundaryWarning ();
+			} else if (other.gameObject.CompareTag ("hazard")) {
+				Destroy (other.gameObject);
+				GameOver ();
+			} else if (other.gameObject.CompareTag ("MyOwnBoundary")) {
+				return;
+			}
 		}
 	}
 
