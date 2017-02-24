c27a04d6eb7b19a7a3c4894d87a8d495156daa0f
wangxin <14302010064@fudan.edu.cn>
2017-02-24 14:47:15 +0800

    debug boundary hazard.

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 	public GameObject hazard;
-	private Vector3 spawnValues = new Vector3 (20,20,50);
+	private Vector3 spawnValues = new Vector3 (20,20,20);
 	public int hazardCount;
 	public float spawnWait;
 	public float startWait;
 	public float waveWait;
 	public Transform target;
-	public GameObject parent;
 
 	void Start ()
 	{
@@ -29,12 +28,11 @@ public class GameController : MonoBehaviour {
 		{
 			for (int i = 0; i < hazardCount; i++)
 			{
-				Vector3 spawnPosition = transform.position + new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), Random.Range (-spawnValues.y, spawnValues.y), spawnValues.z);
+				Vector3 spawnPosition = transform.position + new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), Random.Range (-spawnValues.y, spawnValues.y), Random.Range (-spawnValues.z, spawnValues.z));
 //				Quaternion spawnRotation = Quaternion.identity;
 				Vector3 relativePos = spawnPosition - target.position;
 				Quaternion spawnRotation = Quaternion.LookRotation(relativePos);
-				GameObject relativePosition = Instantiate (parent, transform.position, transform.rotation);
-				Instantiate (hazard, spawnPosition, spawnRotation, relativePosition.transform);
+				Instantiate (hazard, spawnPosition, spawnRotation);
 				yield return new WaitForSeconds (spawnWait);
 			}
 			yield return new WaitForSeconds (waveWait);
